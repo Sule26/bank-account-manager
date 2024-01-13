f31a1f92a45aa945c1978aaa32982cdf0b9bfbbf
Sule26 <alcantara.joaop30@gmail.com>
2024-01-13 20:25:29 -0300

    fix typing

--- src/modules/uris.py
+++ src/modules/uris.py
@@ -1,7 +1,9 @@
+from sqlalchemy.util import immutabledict
 import os
 import psycopg2
 import MySQLdb
 import sqlalchemy
+
 
 POSTGRES_DRIVER = psycopg2.__name__.lower()
 MYSQL_DRIVER = MySQLdb.__name__.lower()
@@ -13,7 +15,7 @@ POSTGRES_URI = sqlalchemy.URL(
     password=os.getenv("POSTGRES_PASSWORD"),
     port=5432,
     database=os.getenv("POSTGRES_DB"),
-    query={"client_encoding": "utf8"},
+    query=immutabledict({"client_encoding": "utf8"}),
 )
 
 
@@ -24,6 +26,6 @@ MYSQL_URI = sqlalchemy.URL(
     password=os.getenv('MYSQL_PASSWORD'),
     port=3306,
     database=os.getenv('MYSQL_DATABASE'),
-    query={"charset": "utf8mb4"},
+    query=immutabledict({"charset": "utf8mb4"}),
 
 )
